ac19fa4e61eaf56e73f91c55533b675487e2a6ea
Usamamalik007 <Usamamalik21@gmail.com>
2022-05-16 02:32:15 +0500

    Update CartScreen.tsx

--- src/screen/CartScreen/CartScreen.tsx
+++ src/screen/CartScreen/CartScreen.tsx
@@ -16,6 +16,8 @@ import AppTextTitle from "../../component/AppTextTitle";
 import { useGetCartRequest } from "../../hooks/Cart/useProductToCart";
 import { ICartDataResponse } from "../../interfaces/ICartData";
 import { useAppSelector } from "../../store/hooks";
+import Moment from "moment";
+
 
 const orders = [
   {
